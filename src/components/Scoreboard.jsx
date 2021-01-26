32a71d13b457bd6cd765e2ce28fb164786e69db3
Sherwin <sherwin.kwan92@gmail.com>
2021-01-26 12:33:33 -0800

    add seven-segment display for score and time

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,18 +1,19 @@
 import PropTypes from 'prop-types';
+import Display from 'seven-segment-display';
 
 const Scoreboard = (props) => {
   return (<>
     <table>
       <thead>
         <tr>
-          <th>Your Score</th>
-          <th>Time Left</th>
+          <th>YOUR SCORE</th>
+          <th>TIME LEFT</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>{props.score}</td>
-          <td>{props.time}</td>
+        <tr className="displays">
+          <td><Display value={String(props.score)} color="green" digitCount={2} /></td>
+          <td><Display value={String(props.time)} color="red" digitCount={2} /></td>
         </tr>
       </tbody>
     </table>
