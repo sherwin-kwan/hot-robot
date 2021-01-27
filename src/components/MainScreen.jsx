024bd20c36c3f03eb146a42745f0229a8b0863e2
Sherwin <sherwin.kwan92@gmail.com>
2021-01-26 16:42:34 -0800

    add footer

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,12 +1,11 @@
 import PropTypes from "prop-types";
 import { boardWidth, boardHeight, initialState } from "../helpers/settings";
 import Square from "./Square";
-import Scoreboard from './Scoreboard';
-import GameOver from './GameOver';
+import Scoreboard from "./Scoreboard";
+import GameOver from "./GameOver";
 
 const MainScreen = (props) => {
-
-  const {state, dispatch} = props;
+  const { state, dispatch } = props;
 
   const squares = (width, height) => {
     const arr = [];
@@ -37,29 +36,34 @@ const MainScreen = (props) => {
           initialState={initialState}
         />
       ) : (
-        <div className="board">{squares(boardWidth, boardHeight)}</div>
+        <>
+          <div className="board">{squares(boardWidth, boardHeight)}</div>
+          <div className="buttons">
+            <input
+              type="image"
+              src="/anticlockwise.png"
+              onClick={() => dispatch({ type: "left" })}
+              title="Turn Left"
+            />
+            <input
+              type="image"
+              className="forward"
+              src="/forward.png"
+              onClick={() => dispatch({ type: "forward" })}
+              title="Forward"
+            />
+            <input
+              type="image"
+              src="/clockwise.png"
+              onClick={() => dispatch({ type: "right" })}
+              title="Turn Right"
+            />
+          </div>
+        </>
       )}
-      <div className="buttons">
-        <input
-          type="image"
-          src="/anticlockwise.png"
-          onClick={() => dispatch({ type: "left" })}
-          title="Turn Left"
-        />
-        <input
-          type="image"
-          className="forward"
-          src="/forward.png"
-          onClick={() => dispatch({ type: "forward" })}
-          title="Forward"
-        />
-        <input
-          type="image"
-          src="/clockwise.png"
-          onClick={() => dispatch({ type: "right" })}
-          title="Turn Right"
-        />
-      </div>
+    <footer>
+      (C) 2021 Sherwin Kwan. Source code available <a href="https://github.com/sherwin-kwan/hot-robot">here</a>
+    </footer>
     </>
   );
 };
@@ -69,5 +73,5 @@ export default MainScreen;
 MainScreen.propTypes = {
   state: PropTypes.object,
   dispatch: PropTypes.func,
-  setLeaders: PropTypes.func
+  setLeaders: PropTypes.func,
 };
